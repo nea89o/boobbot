54a586d295e3069447dfd47e97948a71a791a456
Linnea Gräf <nea@nea.moe>
2025-05-12 21:08:17 +0200

    add deploy

---
+++ create-deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+rm -fr dist
+dub build -b release
+install -Dm755 boobbot -t dist/bin
+mkdir -p dist/rt
+cp -r systemd dist/
+
+cat >dist/install.sh <<"EOF"
+#!/usr/bin/env bash
+(
+cd "$(dirname -- "$0")"
+root="$PWD"
+echo "Switched to $root"
+(
+	cd systemd
+	for f in *; do
+		sed -e "s|SOURCE|$root|g" < "$f" > "/etc/systemd/system/$f"
+	done
+)
+)
+
+EOF
+chmod +x dist/install.sh
